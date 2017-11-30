1e0e4202d151d4e4a5fe4f33bc3220d4ddde91f6
MarianCoru <mcmaryxp@gmai.com>
2017-11-30 01:20:45 -0400

    ela

---
+++ ELA/Profesores/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Profesores.Models;
+
+namespace Profesores.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Profesor> listado = new List<Profesor>();
+        public HomeController()
+        {
+            listado
+                .Add(new Profesor() { rut = 116465561, nombre = "Enrique Salazar Soto", titulo = "Pedagogía en Matemáticas", grado = "Técnico Superior"});
+            listado
+                .Add(new Profesor() { rut = 165644421, nombre = "Martina Gonzalez Toledo", titulo = "Ingeniero en Electrónica", grado = "Ingeniero"});
+        }
+        public IActionResult Mantenedor()
+        {
+            return View(new Profesor());
+        }
+        public IActionResult Listado()
+        {
+            return View(listado);
+        }
+        public IActionResult Ficha(int rut, string nombre, string titulo, string grado)
+        {
+            Profesor nueva = new Profesor();
+
+            return View(nueva);
+        }
+
+    }
+}
---
+++ ELA/Profesores/Models/Profesor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Profesores.Models
+{
+    public class Profesor
+    {
+        public int rut;
+        public string nombre;
+        public string titulo;
+        public string grado;
+    }
+}
